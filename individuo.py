f2df4e357b902715fe7d77f6e1085f08865a180d
sruiNascimento <sruy19@gmail.com>
2022-07-06 20:23:22 -0300

    metodo copia adicionado

--- individuo.py
+++ individuo.py
@@ -1,8 +1,13 @@
 class Individuo:
 
-    def __init__(self):
+    def __init__(self, cromossomo=None):
         self.fitness = 0
-        self.cromossomo = [ [0, 0] ]
+        self.cromossomo = [ [0, 0] ] if not cromossomo else cromossomo
+
+    def copia(self):
+        individuo_temporario = Individuo(self.cromossomo)
+        individuo_temporario.fitness = self.fitness
+        return individuo_temporario
 
     def __repr__(self):
         return f'cromossomo: {self.cromossomo}\nfitness {self.fitness}\n'
