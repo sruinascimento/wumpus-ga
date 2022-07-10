6761f13fc4c10afd17e5ef4d650db94bd77ccb00
sruiNascimento <sruy19@gmail.com>
2022-07-09 21:33:30 -0300

    matriz e agente preparados

--- ambiente.py
+++ ambiente.py
@@ -1,45 +1,95 @@
 from individuo import Individuo
 import numpy as np
-from random import sample
+from random import sample, randint
+
 
 class Ambiente:
 
     def __init__(self, dimensao_quadrada: int):
         self.dimensao_quadrada = dimensao_quadrada
+        self.ouro:int = 10
+        self.wumpus:int = 5
+        self.poco:int = 2
         self.matriz_ambiente: int = self.geraMatrizAmbiente(self.dimensao_quadrada)
-        self.tamanho_da_populacao = 50
+        self.tamanho_da_populacao = 10
         self.populacao = self.geraPopulacao()
         self.melhor_individuo = None
         self.recombinacao_de_cromossomo = 0.9
         self.taxa_de_mutacao = 0.02
-        self.geracao_de_parada = 100
-
+        self.geracao_de_parada = 2
+       
     def inicializa(self):
         self.movimentaPopulacao()
         self.calculaFitnessPopulacao(self.populacao)
         print('Populacao 0')
         print(self.populacao)
         for i in range(self.geracao_de_parada):
-            print(f'Geração {i +1}')
+            print(f'Geração {i + 1}')
             nova_populacao = self.reproduz(self.populacao)
             self.calculaFitnessPopulacao(nova_populacao)
             print(nova_populacao)
 
             print('\n\n\nMUDOU A GERACAO')
             print(self.melhor_individuo)
-       
+
     def geraMatrizAmbiente(self, dimensaoQuadrada: int) -> np.ndarray:
         agente = 1
         ambiente = np.zeros(shape=(dimensaoQuadrada, dimensaoQuadrada))
         ambiente[0][0] = agente
+        ambiente = self.geraCoordenadaPoco(ambiente)
+        ambiente = self.geraCoordenadaOuro(ambiente)
+        ambiente = self.geraCoordenadaWumpus(ambiente)
+
         return ambiente
+
+    def geraCoordenadaPoco(self, matriz_ambiente):
+        lista_coordenadas = []
+        while (len(lista_coordenadas) < self.dimensao_quadrada - 1):
+            coordenada_x, coordenada_y = randint(0, 3), randint(0, 3)
+            if (coordenada_x == 0 and coordenada_y == 0):
+                continue
+            if ([coordenada_x, coordenada_y] not in lista_coordenadas):
+                lista_coordenadas.append([coordenada_x, coordenada_y])
+        for coordenada in lista_coordenadas:
+            x, y = coordenada
+            matriz_ambiente[x][y] = self.poco
+        
+        return matriz_ambiente
+
+    def geraCoordenadaOuro(self, matriz_ambiente):
+        while (True):
+            coordenada_x, coordenada_y = randint(0, 3), randint(0, 3)
+            if coordenada_x == 0 and coordenada_y == 0:
+                continue
+            if matriz_ambiente[coordenada_x][coordenada_y] == self.poco:
+                continue
+            
+            matriz_ambiente[coordenada_x][coordenada_y] = self.ouro
+            break
+        return matriz_ambiente
+    
+    def geraCoordenadaWumpus(self, matriz_ambiente):
+        while (True):
+            coordenada_x, coordenada_y = randint(0, 3), randint(0, 3)
+            if coordenada_x == 0 and coordenada_y == 0:
+                continue
+            if matriz_ambiente[coordenada_x][coordenada_y] == self.poco:
+                continue
+
+            if matriz_ambiente[coordenada_x][coordenada_y] == self.ouro:
+                matriz_ambiente[coordenada_x][coordenada_y]+= self.wumpus
+                break
+            
+            matriz_ambiente[coordenada_x][coordenada_y] = self.wumpus
+            break
+        return matriz_ambiente
 
     def geraPopulacao(self) -> list:
         populacao = [Individuo() for i in range(self.tamanho_da_populacao)]
         return populacao
 
     def movimentaPopulacao(self) -> None:
-        for _ in range(20):
+        for _ in range(19):
             for agente in self.populacao:
                 self.movimentaAgente(agente)
 
@@ -58,16 +108,16 @@ class Ambiente:
         OESTE = 'O'
         x, y = coordenada_atual
 
-        if(movimento == NORTE):
+        if (movimento == NORTE):
             x -= 1
 
-        if(movimento == SUL):
+        if (movimento == SUL):
             x += 1
 
-        if(movimento == LESTE):
+        if (movimento == LESTE):
             y += 1
 
-        if(movimento == OESTE):
+        if (movimento == OESTE):
             y -= 1
 
         return [x, y]
@@ -78,33 +128,43 @@ class Ambiente:
 
     def avaliaOAgente(self, agente: Individuo) -> None:
         '''
-            Alem de avaliar se o agente andou fora do tabuleiro, é necessário avaliar os passos 
+            Alem de avaliar se o agente andou fora do tabuleiro, é necessário avaliar os passos
             válidos. Se ele deu um passo por cada vez, e não se "teleportou".
         '''
-        punicao = -2
+        punicao_fora_tabuleiro = -2
+        punicao_teleporte = -1000
         premiacao = 1
         valor_fitness = 0
         for coordenadas in agente.cromossomo:
             x, y = coordenadas
             if x < 0 or y < 0:
-                valor_fitness += punicao
-            elif x >= self.dimensao_quadrada or y >= self.dimensao_quadrada :
-                valor_fitness += punicao
+                valor_fitness += punicao_fora_tabuleiro
+            elif x >= self.dimensao_quadrada or y >= self.dimensao_quadrada:
+                valor_fitness += punicao_fora_tabuleiro
             else:
                 valor_fitness += premiacao
+
+        for i in range(len(agente.cromossomo) - 1):
+            x, y = agente.cromossomo[i]
+            coordenadas_validas = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
+            proxima_coordenada = agente.cromossomo[i + 1]
+            if proxima_coordenada not in coordenadas_validas:
+                valor_fitness += punicao_teleporte
+
         agente.fitness = valor_fitness
+
         self.verificaMelhorIndividuo(agente)
-    
-    def verificaMelhorIndividuo(self, agente:Individuo) -> None:
+
+    def verificaMelhorIndividuo(self, agente: Individuo) -> None:
         if not self.melhor_individuo:
             self.melhor_individuo = agente
             return
 
         if self.melhor_individuo.fitness < agente.fitness:
             self.melhor_individuo = agente.copia()
-            return        
+            return
 
-    def selecionaIndividuos(self, populacao:list) ->list:
+    def selecionaIndividuos(self, populacao: list) -> list:
         '''
         Seleção pelo método de Torneio
         '''
@@ -113,21 +173,21 @@ class Ambiente:
         for _ in range(total_selecionados):
             selecionados = sample(populacao, 3)
             selecionados.sort(key=lambda individuo: individuo.fitness, reverse=True)
-            piscina.append(selecionados[0])		
-	    
+            piscina.append(selecionados[0])
+
         return piscina
 
     def totalSelecionados(self) -> int:
         return int(self.tamanho_da_populacao * self.recombinacao_de_cromossomo)
 
-    def reproduzIndividuos(self, populacao_selecionada:list) -> list:
+    def reproduzIndividuos(self, populacao_selecionada: list) -> list:
         total_selecionados = self.totalSelecionados()
         novos_filhos = []
         for _ in range(total_selecionados):
             pai, mae = sample(populacao_selecionada, 2)
             filhos = self.geraFilho(pai, mae)
             novos_filhos.extend(filhos)
-        
+
         return novos_filhos
 
     def geraFilho(self, pai, mae) -> tuple:
@@ -145,19 +205,19 @@ class Ambiente:
         # print('2f', segundo_filho)
 
         return (Individuo(primeiro_filho), Individuo(segundo_filho))
-	
-    def mutaIndividuo(self, populacao:list) -> None:
+
+    def mutaIndividuo(self, populacao: list) -> None:
         '''
         Mutando os agentes
         '''
         for agente in populacao:
-            mutacao_atingida = np.random.random() <= self.taxa_de_mutacao 
+            mutacao_atingida = np.random.random() <= self.taxa_de_mutacao
             if mutacao_atingida:
                 self.muta(agente)
 
-    def muta(self, agente:Individuo) -> None:
+    def muta(self, agente: Individuo) -> None:
         '''
-        Bit string mutation 
+        Bit string mutation
         '''
         # print('Agente antes de ser mutado')
         # print(agente)
@@ -165,8 +225,8 @@ class Ambiente:
         posicao_sorteada = np.random.randint(0, tamanho_cromossomo)
         incremento = np.random.random()
         posicao_x = np.random.random() <= 0.5
-        #refatorar para uma função
-        if posicao_x :
+        # refatorar para uma função
+        if posicao_x:
             if incremento > 0.5:
                 agente.cromossomo[posicao_sorteada][0] += 1
             else:
@@ -177,7 +237,7 @@ class Ambiente:
             else:
                 agente.cromossomo[posicao_sorteada][1] -= 1
 
-    def reproduz(self, populacao:list) -> list:
+    def reproduz(self, populacao: list) -> list:
         '''
         realizar a reprodução completa
         '''
@@ -189,5 +249,5 @@ class Ambiente:
         populacao_nova_geracao = nova_populacao + populacao[total_selecionados:]
 
         return populacao_nova_geracao
-     
-            
+
+
